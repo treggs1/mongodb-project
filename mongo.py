34570516f19a98cf1f5657cf8cb7e339ac4ff1df
treggs1 <>
2023-10-21 09:08:34 +0000

    Update mongo.py

--- mongo.py
+++ mongo.py
@@ -22,9 +22,10 @@ conn = mongo_connect(MONGO_URI)
 
 coll = conn[DATABASE][COLLECTION]
 
-documents1 = coll.update_one({"first": "eve"})
+documents1 = coll.update_many(
+    {"nationality": "american"}, {"$set": {"hair_color": "maroon"}})
 
-documents = coll.find()
+documents = coll.find({"nationality": "american"})
 
 for doc in documents:
     print(doc)
